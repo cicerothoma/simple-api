5d13e58a7e5fafb6aad46ddd99f1c14c55d7ea1c
Collins Thomas <thomascollins582@gmail.com>
2021-02-15 02:20:41 +0100

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const cardValidator = require("./controllers/cardValidator");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.options("*", cors());
 
 app.post("/", cardValidator);
 
